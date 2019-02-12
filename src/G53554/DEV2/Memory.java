d888cda308c95023542345cb50d0ebfd2e37f437
justu <justu@192.168.1.37>
2019-02-12 12:32:38 +0100

    DEV2 Program

--- src/G53554/DEV2/Memory.java
+++ src/G53554/DEV2/Memory.java
@@ -5,69 +5,92 @@
  */
 package G53554.DEV2;
 
-import java.util.Arrays;
-
+//import java.util.Arrays;
 /**
  *
  * @author justu
  */
 public class Memory {
 
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
-        int[] memory = new int[20];
+        int[] memory = {1, 1, 0, 0, 5};
+        System.out.println(size(memory, 4));
     }
+
     /**
-     * 
+     * This method search value for the processId in the board and return the postion 
+     * of the value in the board
      * @param memory
      * @param processId
-     * @param fromPs
-     * @return 
+     * @param fromPos
+     * @return la position de la valeur qui est posValeur
      */
-    public static int search(int[]memory,int processId,int fromPs){
+    public static int search(int[] memory, int processId, int fromPos) {
+        int posValeur = 0;
+        for (int i = fromPos; i < memory.length; i++) {
+            if (memory[i] == processId) {
+                posValeur = i;
+            }
+        }
+        return posValeur;
+    }
+
+    /**
+     *This method return the size of any value that repeat it self 
+     * @param memory
+     * @param startPos
+     * @return the size of the value
+     */
+    public static int size(int[] memory, int startPos) {
+        int taillePos = 0;
+        for (int i = startPos; i < memory.length; i++) {
+            while(memory[startPos] == memory[i]) {
+                taillePos++;
+               i++;
+                
+            }
+           
+        }
+        return taillePos;
+    }
+
+    /**
+     *
+     * @param memory
+     * @param wantedSpace
+     * @return
+     */
+    public static int findAvailableSpace(int[] memory, int wantedSpace) {
         
         return 0;
     }
+
     /**
-     * 
-     * @param memory
-     * @param startPos
-     * @return 
-     */
-    public static int size(int[]memory,int startPos){
-        return 0;
-    }
-    /**
-     * 
-     * @param memory
-     * @param wantedSpace
-     * @return 
-     */
-    public static int findAvailableSpace(int[]memory,int wantedSpace){
-        return 0;
-    }
-    /**
-     * 
+     *
      * @param memory
      * @param processId
      * @param processSize
-     * @return 
+     * @return
      */
-    public static boolean load(int[]memory,int processId,int processSize){
+    public static boolean load(int[] memory, int processId, int processSize) {
         return false;
     }
+
     /**
-     * 
+     *
      * @param memory
-     * @param processId 
+     * @param processId
      */
-    public static void remove(int []memory,int processId){
-        
+    public static void remove(int[] memory, int processId) {
+
     }
+
     /**
-     * 
-     * @param memory 
+     *
+     * @param memory
      */
-    public static void compact(int []memory){
-        
+    public static void compact(int[] memory) {
+
     }
 }
